4c24677cc900c393cd8e222e2402f2fdb7047f0f
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-02-07 19:38:48 +0200

    add user email validation

--- models/user.js
+++ models/user.js
@@ -22,6 +22,9 @@ const User = sequelize.define("User", {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: DataTypes.STRING,
