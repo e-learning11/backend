1106c36a14c59050c51e4cfeb4b8f3c1db0a8f8d
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-02-07 23:03:55 +0200

    intial admin controller

---
+++ controllers/admin_controller.js
@@ -0,0 +1,7 @@
+// this the initial admin thought and what functions the admin should handel
+/**
+ * 1 - approve users
+ * 2- approve courses
+ * 3- delete course/user ???
+ * 4- show webiste statistics : no of students/teachers/courses
+ */
