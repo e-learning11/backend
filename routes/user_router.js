553691b9b750e4730016642ef7afc3b0af90f59e
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-02-20 17:00:54 +0200

    add image validation to user signup

--- routes/user_router.js
+++ routes/user_router.js
@@ -2,7 +2,21 @@ const router = require("express").Router();
 const userController = require("../controllers/user_controller");
 const authenticationModule = require("../utils/authentication");
 const multer = require("multer");
-const upload = multer();
+const upload = multer({
+  fileFilter: (req, file, next) => {
+    if (!file) next(null, true);
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      next(null, true);
+    } else {
+      next(null, false);
+      return next(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
 
 router.post("/login", userController.login);
 router.post("/signup", upload.single("image"), userController.signup);
