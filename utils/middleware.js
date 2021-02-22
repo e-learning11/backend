7490320ef20581d3c2c718580e6eccc3c8e6d8b0
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-02-22 23:36:10 +0200

    add check approval middleware

---
+++ utils/middleware.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+const CONSTANTS = require("./const");
+/**
+ * checkUserApproval
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Object} next
+ * check that user is approved from admin
+ */
+async function checkUserApproval(req, res, next) {
+  try {
+    const id = req.user.id;
+    const user = await User.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!user) throw new Error();
+    if (user.type == CONSTANTS.TEACHER && !user.approved) {
+      res
+        .status(400)
+        .send(
+          "Your account is not approved yet by the Admin, please wait till approval and try again later."
+        );
+    } else {
+      next();
+    }
+  } catch (ex) {
+    res.status(400).send("error has occured");
+  }
+}
+
+module.exports = {
+  checkUserApproval,
+};
