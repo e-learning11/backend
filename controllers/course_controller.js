54ff7f800dd3f63c580eaf7c450c7ae5de600e1c
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-02-24 00:12:25 +0200

    make admin get users enrolled in course

--- controllers/course_controller.js
+++ controllers/course_controller.js
@@ -2389,7 +2389,12 @@ async function getCourseEnrolledUsers(req, res) {
         type: CONSTANTS.CREATED,
       },
     });
-    if (!userCourse)
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    if (!userCourse && user.type != CONSTANTS.ADMIN)
       throw new Error(
         JSON.stringify({ errors: [{ message: "user not owner of course" }] })
       );
